f3c406afd47b09237fd8785eddb3d53f942c6e01
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-01-10 20:35:07 +1100

    variables

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 //Variables
-
-
+var searchButton = $("#search-button");
+var clearButton = $("#clear-search");
+var searchCity = $("#search-city");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidity = $("#humidity");
+var currentWindSpeed = $("#wind-speed");
+var currentUvIndex = $("#uv-index");
+var cities = [];
+var city = "";
+var searchButton = $();
 var apiKey = "0285f9ca23eb9733a78696b08ba93c18";
+
