59908cbad1162a6babafabf4bad701a1ce732dd2
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-01-11 12:03:42 +1100

    added variable values to IDs

--- script.js
+++ script.js
@@ -111,10 +111,10 @@ function forecast(cityid){
             var tempFar = (((tempKel-273.5)*8)+32).toFixed(2);
             var humidity = response.list[((i+1)*8)-1].main.humidity; 
 
-            $("#forDate"+i);
-            $("#forImg"+i);
-            $("#forTemp"+i);
-            $("#forHumidity"+i);
+            $("#forDate"+i).html(date);
+            $("#forImg"+i).html("<img src="+iconUrl+">");
+            $("#forTemp"+i).html(tempFar+"&#8457");
+            $("#forHumidity"+i).html(humidity+"%");
         }
         });
   
